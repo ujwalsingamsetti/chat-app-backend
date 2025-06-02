f1a64ef4f4581b008398bf2998f3832bf7ce745c
ujwal singamsetti <ujwalsingamsetti@ujwal.local>
2025-06-02 15:25:08 +0530

    Updated server code for deployment

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ app.use(cors({
 }));
 
 // MongoDB Connection
+if (!process.env.MONGO_URI) {
+  console.error('MONGO_URI is not defined in environment variables');
+  process.exit(1);
+}
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
